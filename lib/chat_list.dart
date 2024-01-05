5d412959f67d4d5b3b8007a470b0fc241d6e697c
Theo Breaux <theobreaux@gmail.com>
2024-01-04 21:15:56 -0800

    adding state

---
+++ lib/chat_list.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:front_end_evaluation/background_color_container.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
+
+class ChatList extends StatefulWidget {
+  const ChatList({super.key});
+
+  @override
+  State<ChatList> createState() {
+    return _ChatList();
+  }
+}
+
+class _ChatList extends State<ChatList> {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: const Color.fromRGBO(28, 28, 29, 1),
+          leading: TextButton(
+            onPressed: () {
+              print('Edit Message!');
+            },
+            child: const Text(
+              "Edit",
+              style: TextStyle(
+                color: Color.fromRGBO(149, 161, 172, 1),
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          title: GradientText(
+            "Inbox",
+            style: const TextStyle(
+              fontSize: 20,
+            ),
+            colors: const [
+              Color.fromRGBO(198, 95, 253, 1),
+              Color.fromRGBO(112, 157, 255, 1),
+            ],
+          ),
+          actions: <Widget>[
+            ShaderMask(
+              blendMode: BlendMode.srcIn,
+              shaderCallback: (Rect bounds) => const LinearGradient(
+                colors: [
+                  Color.fromRGBO(173, 45, 242, 0.75),
+                  Color.fromRGBO(62, 121, 255, 0.75),
+                  Color.fromRGBO(62, 121, 255, 0.75),
+                ],
+              ).createShader(bounds),
+              child: IconButton(
+                icon: const Icon(Icons.search),
+                tooltip: 'Search',
+                iconSize: 30,
+                onPressed: () {
+                  print('Search For Shit!');
+                },
+              ),
+            )
+          ],
+        ),
+        body: const BackgroundColorContainer(),
+        bottomNavigationBar: BottomNavigationBar(
+          selectedFontSize: 0,
+          // currentIndex: 2,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home_outlined,
+                size: 30,
+                color: Color.fromRGBO(149, 161, 172, 1),
+              ),
+              label: '',
+              backgroundColor: Color.fromRGBO(28, 28, 29, 1),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.graphic_eq_rounded,
+                size: 30,
+                color: Color.fromRGBO(149, 161, 172, 1),
+              ),
+              label: '',
+              backgroundColor: Color.fromRGBO(28, 28, 29, 1),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                size: 30,
+                color: Color.fromRGBO(149, 161, 172, 1),
+              ),
+              label: '',
+              backgroundColor: Color.fromRGBO(28, 28, 29, 1),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.mail_outline_sharp,
+                size: 30,
+                color: Color.fromRGBO(149, 161, 172, 1),
+              ),
+              label: '',
+              backgroundColor: Color.fromRGBO(28, 28, 29, 1),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person_outline,
+                size: 30,
+                color: Color.fromRGBO(149, 161, 172, 1),
+              ),
+              label: '',
+              backgroundColor: Color.fromRGBO(28, 28, 29, 1),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/chat_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
+  @override
+  State<ChatPage> createState() {
+    return _ChatPageState();
+  }
+}
+
+class _ChatPageState extends State<ChatPage> {
+  @override
+  Widget build(context) {
+    return const Text('ChatPageScreen');
+  }
+}
